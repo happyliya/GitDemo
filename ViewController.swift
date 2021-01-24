ab2f2af2d8136987f2f64a47c7e2782b433e4a35
happyliya <nat335@yandex.ru>
2021-01-24 17:07:50 +0300

    Print for test merge conflict

--- ViewController.swift
+++ ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Pull Test")
+        print("I dont want to print anything")
         // Do any additional setup after loading the view.
     }
 
